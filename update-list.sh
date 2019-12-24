cb5c14744a21fc5e8f79cdb38d31f6863b412212
sajinsa99 <bfablet@gmail.com>
2019-12-24 20:55:47 +0100

    Mar déc 24 20:55:43 CET 2019

--- update-list.sh
+++ update-list.sh
@@ -6,6 +6,18 @@ cd /root/
 . .bashrc
 
 cd /biblio-mam/
-git pull
+
+git pull ; git status
+echo
+
 perl -w list.pl > results.txt
 cat results.txt
+echo
+git status
+echo
+
+current_date=`date`
+git commit -am "$current_date"
+git push
+git status
+
